8d26fc39a008c1a3fc268ca81e3c946a516764f7
Maelkum <aleksandar.cekrlic@gmail.com>
2024-11-12 16:26:21 +0100

    Add a counter utility to test long-lived executions

---
+++ exp/counter/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/spf13/pflag"
+)
+
+func main() {
+
+	var (
+		flagCount         uint
+		flagSleepDuration time.Duration
+	)
+
+	pflag.UintVar(&flagCount, "count", 60*10, "how many loops will the program do")
+	pflag.DurationVar(&flagSleepDuration, "sleep-duration", time.Second, "pause between loops")
+
+	pflag.Parse()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	go func() {
+		<-ch
+		log.Printf("received a SIGINT!")
+		os.Exit(1)
+	}()
+
+	for i := uint(0); i < flagCount; i++ {
+		fmt.Printf("stdout #%v\n", i)
+		log.Printf("stderr #%v\n", i)
+
+		time.Sleep(flagSleepDuration)
+	}
+}
